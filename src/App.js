2b93bd146e80fac83c5754eba15032c5055ab3ed
Vicky Min <vicky.w.min@gmail.com>
2020-12-13 20:16:32 -0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import pic from './pic.png';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+var ans = '';
+
+function handleChange(event) {
+  ans = event.target.value;
+  return event.target.value;
+}
+
+function show() {
+  if(ans == '2') {
+    alert("You got the secret code! Polynomials has $1,024,311.25 in funds.");
+  } else {
+    alert("Sorry, that is not the correct secret code. Please try again.");
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><b>WELCOME TO POLYNOMIAL INC'S BANK ACCOUNT.</b></h1>
+      <div classname="logo">
+        <img src={pic} width="250" height="250" />
+      </div>
+      <p>
+      To access funds, please type in the secret code.</p>
+      <TextField id="standard-basic" onChange={handleChange}/>
+      <Button onClick={show} variant="contained" color="primary">
+        Enter
+      </Button>
+      <p id="amountt"></p>
     </div>
   );
 }
